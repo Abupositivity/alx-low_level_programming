b64390a2ff562a230de703c8b008312223484d83
Mukhtar Salihu <abupositivity@gmail.com>
2022-12-12 09:29:09 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,13 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)  
+int main(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
-for (c = 'a'; c <= 'z'; c++)
+for (c = 'A'; c <= 'Z'; c++)
 putchar(c);
-putchar('\n'); 
-return (0);  
+putchar('\n');
+return (0);
 }
