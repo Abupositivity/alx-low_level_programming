311da08464f27c3843d130aeb82d82b1a5d3c2e7
Mukhtar Salihu <abupositivity@gmail.com>
2022-12-16 10:55:56 -0800

    Task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,18 @@ n = 1;
 printf("%d", n);
 for (n = 2; n <= 100; n++)
 {
-if ((n % 3 == 0) && (n % 5 == 0))
+if
+((n % 3 == 0) && (n % 5 == 0))
 {
 printf(" FizzBuzz");
 }
-else if (n % 3 == 0)
+else if
+(n % 3 == 0)
 {
 printf(" Fizz");
 }
-else if (n % 5 == 0)
+else if
+(n % 5 == 0)
 {
 printf(" Buzz");
 }
