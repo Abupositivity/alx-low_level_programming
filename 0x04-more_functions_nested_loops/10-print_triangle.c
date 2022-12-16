46cf902c0699c853be7458b4af0506ed2c09ed14
Mukhtar Salihu <abupositivity@gmail.com>
2022-12-16 11:00:29 -0800

    Task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,35 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* main - prints Buzz each numbers of 3 and 5.
-* Return: Always 0.
+* print_triangle - prints a triangle.
+* @size: numbers of lines.
+* Return: no return.
 */
-int main(void)
+void print_triangle(int size)
 {
-int n;
-n = 1;
-printf("%d", n);
-for (n = 2; n <= 100; n++)
+int i, j;
+for (i = 0; i < size; i++)
 {
+for (j = 1; j < (size - i); j++)
+_putchar(' ');
+for (j--; j < size; j++)
+_putchar(35);
 if
-((n % 3 == 0) && (n % 5 == 0))
-{
-printf(" FizzBuzz");
+(i < (size - 1))
+_putchar('\n');
 }
-else if
-(n % 3 == 0)
-{
-printf(" Fizz");
-}
-else if
-(n % 5 == 0)
-{
-printf(" Buzz");
-}
-else
-{
-printf(" %d", n);
-}
-}
-printf("\n");
-return (0);
+_putchar('\n');
 }
